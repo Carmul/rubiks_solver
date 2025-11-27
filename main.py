3dbbb47feefd5fc6db4ebacb84fc525bc127e0e3
shaked <shaked.sasson07@gmail.com>
2025-11-27 20:23:19 +0200

    build_heuristic_db

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from Cube import Cube
+import tqdm
+all_actions = [['h',0,0],['h',0,1],['h',2,0],['h',2,1],['v',0,0],['v',0,1],['v',2,0],['v',2,1],['s',0,0],['s',0,1],['s',2,0],['s',2,1]]
 
-c = Cube()
-
-c.pprint()
-
-print(c.is_solved())
-
-c.turn("R")
-
-c.pprint()
-
-print(c.is_solved())
-
+def build_heuristic_db(state, actions, max_moves = 20, heuristic = None):
+    if heuristic is None:
+        heuristic = {state: 0}
+    que = [(state, 0)]
+    node_count = sum([len(actions) ** (x + 1) for x in range(max_moves + 1)])
+    with tqdm(total=node_count, desc='Heuristic DB') as pbar:
+        while True:
+            if not que:
+                break
+            s, d = que.pop()
+            if d > max_moves:
+                continue
+            for a in actions:
+                cube = Cube(s)
+                if a[0] == 'h':
+                    cube.horizontal_twist(a[1], a[2])
+                elif a[0] == 'v':
+                    cube.vertical_twist(a[1], a[2])
+                elif a[0] == 's':
+                    cube.side_twist(a[1], a[2])
+                a_str = cube.stringify()
+                if a_str not in heuristic or heuristic[a_str] > d + 1:
+                    heuristic[a_str] = d + 1
+                que.append((a_str, d+1))
+                pbar.update(1)
+    return heuristic
